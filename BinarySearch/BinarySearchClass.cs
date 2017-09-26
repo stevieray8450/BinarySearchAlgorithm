78000e1d56987242de5844be92f95088b5103087
Stephen Boniface <stevieray8450@gmail.com>
2017-09-25 20:41:23 -0500

    "first commit"

---
+++ BinarySearch/BinarySearchClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace BinarySearch
+{
+    public static class BinarySearchClass
+    {
+        public static int[] MyArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        public static int Search(int[] array) 
+        {
+            int start, mid, end = 0;
+            int arrLength = MyArray.Length;
+			end = arrLength - 1;
+			mid = end / 2;
+            bool valueFound = false;
+
+            while (!valueFound)
+            {
+                if (array[mid] == 1)
+                {
+                    valueFound = true;
+                    return mid;
+                }
+                else if (array[mid] > 1)
+                {
+                    end = mid;
+                    mid = end / 2;
+                }
+                else if (array[mid] < 1)
+                {
+                    start = mid;
+                    mid = (start + end) / 2;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(BinarySearchClass.Search(BinarySearchClass.MyArray).ToString());
+        }
+    }
+}
